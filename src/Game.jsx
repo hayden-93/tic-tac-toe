8c4114d46a9d3e0f4346b4b86b709d2e3971e78a
Hayden Buckley-Smith <hayden@abstack.co>
2022-05-03 14:38:58 +0100

    add handle click event and restart button to finish the game

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Square = (props) => {
   return (
     <>
-      <button className="square" onClick={() => {}}>
+      <button className="square" onClick={props.onClickEvent}>
         {props.value}
       </button>
     </>
@@ -11,17 +11,46 @@ const Square = (props) => {
 };
 
 const Board = () => {
-  const initialSquares = [null, null, null, null, null, null, null, null, null];
+  const initialSquares = Array(9).fill(null);
   const [squares, setSquares] = useState(initialSquares);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClickEvent = (i) => {
+    // 1. make a copy of the squares state array
+    // 2. mutate the copy setting the i element to 'X'
+    // 3. call the setSquare function with the mutated copy
+    const newSquares = [...squares];
+
+    const winnerDeclared = Boolean(calculateWinner(newSquares));
+    const squareFilled = Boolean(newSquares[i]);
+    if (winnerDeclared || squareFilled) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  function restart() {
+    setSquares(Array(9).fill(null));
+  }
 
   const renderSquare = (i) => {
-    return <Square value={squares[i]} />;
+    return (
+      <Square value={squares[i]} onClickEvent={() => handleClickEvent(i)} />
+    );
   };
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   return (
     <>
-      <div style={{ backgroundColor: "skyblue", margin: 10, padding: 20 }}>
-        Board
+      <div>
+        <div className="status">{status}</div>
         <div className="board-row">
           {renderSquare(0)}
           {renderSquare(1)}
@@ -37,15 +66,42 @@ const Board = () => {
           {renderSquare(7)}
           {renderSquare(8)}
         </div>
+        <div>
+          <button className="restart" onClick={restart}>
+            Play a new game
+          </button>
+        </div>
       </div>
     </>
   );
 };
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // columns
+    [0, 4, 8],
+    [2, 4, 6], // diagonals
+  ];
+
+  for (let line of lines) {
+    const [a, b, c] = line;
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Game() {
   return (
     <div className="game">
-      Game
+      Tic-Tac-Toe
       <Board />
     </div>
   );
