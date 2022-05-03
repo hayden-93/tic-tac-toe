ba6d194ddf4bf77f2806b9be392485d410209814
Hayden Buckley-Smith <hayden@abstack.co>
2022-05-03 10:27:18 +0100

    create board and square functions to pass props to each individual square

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+const Square = (props) => {
+  return (
+    <>
+      <div className="square">{props.value}</div>
+    </>
+  );
+};
+
+const Board = () => {
+  const renderSquare = (i) => {
+    return <Square value={i} />;
+  };
+
+  return (
+    <>
+      <div style={{ backgroundColor: "skyblue", margin: 10, padding: 20 }}>
+        Board
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </>
+  );
+};
+
 function Game() {
-  return <div className="">Game</div>;
+  return (
+    <div className="game">
+      Game
+      <Board />
+    </div>
+  );
 }
 
 export default Game;
